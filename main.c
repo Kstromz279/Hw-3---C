a836dfa09b5c08895f9eec9b13b67d69db3cfd14
Khalil Stroman <7208392@philasd.org>
2020-09-18 22:37:43 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,20 @@
+// Author: Khalil Stroman kzs5955@psu.edu
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+int digit_sum(int n){
+  if(n>0){
+  return n%10 + digit_sum(n/10);
+  }
+  else{
+    return 0;
+  }
+}
+
+
+int main(void) {
+  char *digits = readline("Enter an int: ");
+  int num = atoi(digits);
+  printf("sum of digits is %i.\n",digit_sum(num));
+  return 0;
+}
